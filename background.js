bbfbf18bebf3389c5bde38e3d9b2eb429b093ca4
Mathias Bynens <mathias@qiwi.be>
2014-04-01 12:21:30 +0200

    Fix formatting

--- background.js
+++ background.js
@@ -1,2 +1,4 @@
-chrome.tabs.create({"url": "http://operasoftware.github.io/wmue/inter.html"});
+chrome.tabs.create({
+	"url": "http://operasoftware.github.io/wmue/inter.html"
+});
 chrome.management.uninstallSelf();
